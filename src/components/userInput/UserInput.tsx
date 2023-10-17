d2ae691f56be916b078eab3f8eb0d5d052c57be6
GiorgiaPietroluongo <142852498+GiorgiaPietroluongo@users.noreply.github.com>
2023-10-16 21:58:23 -0300

    ok

--- src/components/userInput/UserInput.tsx
+++ src/components/userInput/UserInput.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import'./UserInput.css';
+import  axios from "axios";
 
 
-function userData(){
+async function userData(){
     var userNameInput = document.getElementById("userName")as HTMLInputElement || null;
     var userLastNameInput = document.getElementById("userLastName")as HTMLInputElement || null;
     var userPhoneInput = document.getElementById("userPhone")as HTMLInputElement || null;
@@ -10,30 +11,55 @@ function userData(){
     var userEmailInput = document.getElementById("userEmail")as HTMLInputElement || null;
     var userCpfInput = document.getElementById("userCpf")as HTMLInputElement || null;
     var newDiv = document.getElementById("newDiv")as HTMLDivElement;
+    var vendaUrl = "http://localhost:3000/api/sales";
+    var userDataJson = {
 
-    newDiv.innerHTML = `
-        <div>
-        Nome: ${userNameInput.value}
-        </div>
-        <div> 
-        Sobrenome: ${userLastNameInput.value}
-        </div>
-        <div> 
-        Telefone: ${userPhoneInput.value}
-        </div>
-        <div> 
-        Endereço: ${userAddressInput.value}
-        </div>
-        <div>
-        Email: ${userEmailInput.value} 
-        </div>
-        <div>
-        CPF: ${userCpfInput.value} 
-        </div>
-    `;
+        "name": userNameInput.value.toString(),
+        "products": userLastNameInput.value.toString(),
+        "finishedSales": userPhoneInput.value.toString(),
+        "discount": userAddressInput.value.toString(),
+        "Sale": userEmailInput.value.toString()
+     
+    }
 
+    // newDiv.innerHTML = `
+    //     <div>
+    //     Nome: ${userNameInput.value}
+    //     </div>
+    //     <div> 
+    //     Sobrenome: ${userLastNameInput.value}
+    //     </div>
+    //     <div> 
+    //     Telefone: ${userPhoneInput.value}
+    //     </div>
+    //     <div> 
+    //     Endereço: ${userAddressInput.value}
+    //     </div>
+    //     <div>
+    //     Email: ${userEmailInput.value} 
+    //     </div>
+    //     <div>
+    //     CPF: ${userCpfInput.value} 
+    //     </div>
+    // `;
 
+    // axios.post(salesPersonUrl,userDataJson )
+    // .then((response)=>{
+    //     newDiv.innerHTML = `${response.data}`;
+    // })
+
+    await axios.get(vendaUrl, 
+       { headers:{
+            "Content-type":"application/json",
+            Accept:"application/json"
+             }
+         }
+     ).then ((response) =>{
+        newDiv.innerHTML = `${response.data}`;
+    })
 }
+
+
 function UserInput(){
     return(
         <div className="userInputMain">
